23a51de971e51f2dec398aa053ad09f62ad63fe3
Anitha <71427001+anianitha835@users.noreply.github.com>
2022-09-15 12:43:44 +0530

    Time: 219 ms (44.16%), Space: 143.1 MB (38.47%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        int len=changed.length;
+        if((len&1)==1) return new int[0];
+        Arrays.sort(changed);      
+        Map<Integer,Integer> hm=new HashMap<>();
+         
+        int res[] = new int[len/2];
+        int j=0;
+        for(int i=0;i<len;i++){
+            int ele=changed[i];
+            hm.put(ele,hm.getOrDefault(ele,0)+1);
+        }
+     
+        for(int i=0;i<len;i++){
+            int ele=changed[i];
+            if(hm.containsKey(ele)){
+                if(hm.containsKey(ele*2)){
+                    res[j++]=ele;
+                    
+                    
+                    hm.put(ele,hm.get(ele)-1);
+                    hm.put(ele*2,hm.get(ele*2)-1);
+                    
+                    if(hm.get(ele)<=0) hm.remove(ele);
+                    if(hm.containsKey(ele*2)&& hm.get(ele*2)<=0) hm.remove(ele*2);
+                }
+            
+            else return new int[0];
+            }
+            
+            }
+            
+         return res;
+            }
+        
+       
+        }
+        
+ 
