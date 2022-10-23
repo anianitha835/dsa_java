97b19cab4f8512d29ca1b7905312dafff0bae36f
Anitha <71427001+anianitha835@users.noreply.github.com>
2022-10-23 18:35:17 +0530

    Added solution - LeetHub

---
+++ Stepping Numbers - GFG/stepping-numbers.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int  N = Integer.parseInt(input_line[0]);
+            int  M = Integer.parseInt(input_line[1]);
+            
+            Solution ob = new Solution();
+            int ans = ob.steppingNumbers(N, M);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int steppingNumbers(int n, int m){
+        // code here
+        int count=0;
+        for(int i=0;i<=9;i++){
+            count+=bfs(i,n,m);
+        }
+        return count;
+    }
+    
+    private int bfs(int i,int n,int m){
+        int c=0;
+        Queue<Integer> q=new LinkedList<>();
+        q.add(i);
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            
+            if(curr>=n &&curr<=m){
+                c++;
+            }
+            if(curr==0 || curr>m){
+                continue;
+            }
+            int rem=curr%10, n1;
+            
+            if(rem!=9){
+                n1=curr*10+rem+1;
+                q.add(n1);
+            }
+            if(rem!=0){
+                n1=curr*10+rem-1;
+                q.add(n1);
+            }
+        }
+        return c;
+    }
+}
