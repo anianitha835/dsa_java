01cb1dab1d65c925c8759a5b636e9eb6530247db
Anitha <71427001+anianitha835@users.noreply.github.com>
2022-11-22 17:39:09 +0530

    Time: 2 ms (38.58%), Space: 41.9 MB (72.20%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -14,16 +14,18 @@ class Solution {
        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                
                 if(grid[i][j]==1)
                     dp[i][j]=0;
                 else if(i==0 && j==0)
-                    dp[0][0]=1;
-                else if(i==0)
-                dp[i][j]=dp[i][j-1];
-                else if(j==0)
-                    dp[i][j]=dp[i-1][j];
-                else 
-                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                    dp[i][j]=1;
+                
+                else{
+                    int l=0,up=0;
+                    if(j>0) up=dp[i][j-1];
+                    if(i>0) l=dp[i-1][j];
+                   dp[i][j]=up+l;
+                }
             }
         }
          
