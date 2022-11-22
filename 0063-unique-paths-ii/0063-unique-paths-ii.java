62219250d8112ecd9a045651f63b6df925b74778
Anitha <71427001+anianitha835@users.noreply.github.com>
2022-11-22 17:29:03 +0530

    Time: 2 ms (38.58%), Space: 41.9 MB (72.20%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -6,19 +6,29 @@ class Solution {
         for(int row[]: dp){
             Arrays.fill(row,-1);
         }
-        return findPaths(r-1,c-1,obstacleGrid,dp);
+        return findPaths(obstacleGrid,dp,r,c);
     }
-    private int findPaths(int i,int j,int[][] grids,int dp[][]){
-        if(i<0 ||j<0) return 0;
-        if(grids[i][j]==1) return 0;
-         if(i==0 &&j==0) return 1;
+    private int findPaths(int[][] grid,int dp[][],int n,int m){
+       
         
-         if(dp[i][j]!=-1) return dp[i][j];
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)
+                    dp[i][j]=0;
+                else if(i==0 && j==0)
+                    dp[0][0]=1;
+                else if(i==0)
+                dp[i][j]=dp[i][j-1];
+                else if(j==0)
+                    dp[i][j]=dp[i-1][j];
+                else 
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+         
         
-         int up=findPaths(i-1,j,grids,dp);
-         int left=findPaths(i,j-1,grids,dp);
-        
-        return dp[i][j]=up+left;
+        return dp[n-1][m-1];
         
     }
 }
