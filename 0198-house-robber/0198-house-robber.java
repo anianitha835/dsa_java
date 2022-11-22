90b8048813a6b91ee741ceccd9feecc44d183d00
Anitha <71427001+anianitha835@users.noreply.github.com>
2022-11-22 15:26:51 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (91.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,7 +2,7 @@ class Solution {
   
     public int rob(int[] nums) {
         int n=nums.length;
-        int dp[]=new int[n];
+        int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
         return helper(n-1,nums,dp);
     }
