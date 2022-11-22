90c0deb42d6f90d009ea11659adefab7423f0def
Anitha <71427001+anianitha835@users.noreply.github.com>
2022-11-22 15:26:32 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (82.33%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+  
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return helper(n-1,nums,dp);
+    }
+    private int helper(int ind,int arr[],int dp[]){
+        if(ind==0) return arr[ind];
+        if(ind<0) return 0;
+        
+        if(dp[ind]!=-1) return dp[ind];
+        int rob=arr[ind]+helper(ind-2,arr,dp);
+        int notRob = helper(ind-1,arr,dp);
+        return dp[ind]=Math.max(rob,notRob);
+        
+    }
+   
+}
